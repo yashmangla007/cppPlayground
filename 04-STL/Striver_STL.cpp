c04da0117642642d103dbac6babb64b30c8c12f3
yashmangla007 <yashmangla.official@gmail.com>
2026-05-26 13:09:21 +0530

    Commit

--- 04-STL/Striver_STL.cpp
+++ 04-STL/Striver_STL.cpp
@@ -1,5 +1,5 @@
 // -----------------------------~Pairs~--------------------
-
+/*
 #include<bits/stdc++.h> // Includes all the libraries
 using namespace std;
 
@@ -28,6 +28,34 @@ int main(){
         cout<<student[i].first<<" -> "<<student[i].second<<endl;
     }
     
+
+
+    return 0;
+}*/
+
+// -----------------------------~VECTORS~--------------------
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+        vector<int> v1;
+        v1.push_back(21); // v1 = {21}
+        v1.emplace_back(122); // v1 = {21, 122}
+
+        vector<int> v2 (5); //      v2 = {__ , __ , __ , __ , __}
+        vector<int> v3(4, 20); //   v3 = {20,20,20,20,20}
+        v3.emplace_back(2);
+        cout<<v3[4];
+        
+        vector<int> v2(v3); //      v2 = {20,20,20,20,20}
+ 
+    // using pairs as a data type in Vector:
+
+    vector<pair<int, int>> v4;
+    v4.push_back({12,11}); // v2 = {{12,11}}
+    v4.emplace_back(32,23);// v2 = {{12,11}, {32,23}}
 
 
     return 0;
