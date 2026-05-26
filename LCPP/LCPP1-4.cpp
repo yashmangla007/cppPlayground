5abf4a721351692cb69866e6d66ee272147f6105
yashmangla007 <yashmangla.official@gmail.com>
2026-05-26 15:01:07 +0530

    LCPP-1.2

--- LCPP/LCPP1-4.cpp
+++ LCPP/LCPP1-4.cpp
@@ -6,6 +6,14 @@
 /*
 *Theory*
 -> When we see curly braces, we know we’re list-initializing an object.
+-> NARROWING CONVERSION: if we use int __ = 3.5, then if compiler senses it as int __ = 3, then it is Narrowing conversion.
+-> About unused variables:
+    sometimes, we dont use variables into anything, we just define and iniitalise them but not use
+    eg: double pi (3.14);
+    now, compiler might flag them as warnings or even errors (if warning ^ errors)
+    thus use:
+    [[maybe_unused]] double pi (3.14);
+
 */
 
 //5 common types of intialisation:
@@ -16,15 +24,18 @@ int main()
 
     int a; // default-initialization (no initializer)
 
-    // Traditional initialization forms:
-    int b = 5; /* copy-initialization (initial value after equals sign)
+    // Traditional initialization forms: 
+    int b = 5; /* copy-initialization (initial value after equals sign) (ALLOWS NARROWING CONVERSIONS)
                   copy-initialization is also used whenever values are implicitly copied, such as when passing arguments to
                   a function by value, returning from a function by value, or catching exceptions by value.*/
-    int c(6);  // direct-initialization (initial value in parenthesis)
+    int c(6);  // direct-initialization (initial value in parenthesis) (ALLOWS NARROWING CONVERSIONS)
 
-    // Modern initialization forms (preferred):
+    // Modern initialization forms (preferred):  (DONOT ALLOW NARROWING CONVERSIONS)
     int d{7}; // direct-list-initialization (initial value in braces)
     int e{};  // value-initialization (empty braces)
+
+    //Using Maybe Unused
+    [[maybe_unused]] float pi (3.14);
 
     std::cout<<"b="<<b ;
 
---
+++ LCPP/LCPP1-5.cpp
@@ -0,0 +1,66 @@
+/***********************************************Learning CPP from CPP - Chapter 1*************************/
+
+
+                          /***********1.5 — Introduction to iostream: cout, cin, and endl**********/
+
+/*
+                                                            *Theory*
+-> << = Insertion operator
+-> >> = Extraction Operator
+
+-> Buffer: When a statement wants to send the output to the console, it is not sent to console immediately. Instead, the requested 
+           output “gets in line”, and is stored in a region of memory set aside to collect such requests (called a buffer).
+           Periodically, the buffer is flushed, meaning all of the data collected in the buffer is transferred to its destination 
+           (in this case, the console). 
+
+-> USE \n INSTEAD OF std::endl --> because: 1. endl uses multiple flushes | 2. slower to write
+-> in general, use "\n" and not '\n', although, both are true          
+
+-> Adding data to the end of a buffer and removing it from the front of a buffer ensures data is processed in the 
+   same order in which it was added. This is sometimes called FIFO (first in, first out).
+   - THUS, cin: as Characters entered, are stored in end of input buffer. Also, final enter is stored as \n 
+   - The extraction operator ‘>>’ removes characters from the front of the input buffer and converts them into a value that is 
+     assigned (via copy-assignment) to the associated variable. This variable can then be used in subsequent statements.
+
+-> Hota kya hai Step by Step: CODE: std::cin>>X;
+    1. Compiler reads "std::cin" => creates a buffer such that. Eg: if i wrote 1234, then 1 will be added, then 2, then 3, then 4.....
+                                    until encoutered with an ENTER
+    2. Compiler reads ">>" => Now it extarcts the value from the buffer, like 1 is extracted, 
+                              then 2 then 3......upto encoutered with a whitespace character (spaces, enter etc..)
+    3. Compiler reads "X" => The extracted value is added to X variable via copy-assignment. 
+    -> Conflict case: If I enter input as : 1 6 --> this will be stored in buffer as: 1 6\n
+                      now, >> will take as 1 from buffer, leaving 6 for later extraction, and when the next >> is used, it will 
+                      automatically take value 6 without even checking that repective cin value.
+
+*/
+
+
+#include <iostream>
+
+int main(){
+
+    std::cout<<"Hi,";
+    std::cout<<"My name is Yash.";      // not used any next line or new line thing.
+
+
+    std::cout<<"\nHi,"<<std::endl;
+    std::cout<<"My name is Yash."<<std::endl;
+
+    std::cout<<"Hi,\n";
+    std::cout<<"My name is Yash"<<'\n';
+    
+    //understading cin and >> 
+    // (run both below block 1 and 2, firslty with Block 1 commented out, then not commented out)
+
+    std::cout<<"\nEnter your age: ";        
+    int age;                                 
+    std::cin>>age;
+    std::cout<<"Your age is: "<<age;
+
+    std::cout<<"\nEnter your age with a space: "; 
+    int x; 
+    std::cin>>x;
+    std::cout<<"\nAge: "<<x;
+
+    return 0;
+}
