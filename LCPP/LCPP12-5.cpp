31a2c412ebcc6c1ecc0262b8bb785452d127e142
yashmangla007 <yashmangla.official@gmail.com>
2026-05-27 20:28:39 +0530

    LCPP_D2-3

--- LCPP/LCPP12-5.cpp
+++ LCPP/LCPP12-5.cpp
@@ -6,8 +6,20 @@
 /*
                                                             *Theory*
 
--> 
-
+-> When to use or not use Pass by Value or Reference?
+    -> 2 major costs are involved while passing to a function: cost of copying/binding, cost of accessing
+    -> Each time a value parameter is used, the running program can directly access the storage location 
+       (CPU register or RAM) of the copied argument. However, when a reference parameter is used, there 
+       is usually an extra step. The running program must first directly access the storage location 
+       (CPU register or RAM) allocated to the reference, in order to determine which object is being referenced. 
+       Only then can it access the storage location of the referenced object (in RAM).
+    -> Therefore, each use of a value parameter is a single CPU register or RAM access, whereas each use of a 
+       reference parameter is a single CPU register or RAM access plus a second RAM access.
+    -> Third, the compiler can sometimes optimize code that uses pass by value more effectively than code that 
+       uses pass by reference.
+-> NOTE:==> -> For objects that are cheap to copy, the cost of copying is similar to the cost of binding, 
+               but accessing the objects is faster and the compiler is likely to be able to optimize better.
+            -> For objects that are expensive to copy, the cost of the copy dominates other performance considerations.
 */
 
 #include <iostream>
